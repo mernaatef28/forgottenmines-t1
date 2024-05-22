022164b111f0594cecf6376a2f8c85e54cf9f7fc
merna atef <142946454+mernaatef28@users.noreply.github.com>
2024-05-22 11:50:54 +0300

    22/5

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert ;
+
+
+
+  class Article {
+  final String title;
+  final String description;
+
+  Article({required this.title, required this.description});
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+      title: json['title'] ?? 'No Title',
+      description: json['description'] ?? 'No Description',
+    );
+  }
+  }
+
+
+class NewsApp extends StatelessWidget {
+  Future<List<Article>> fetchNews() async {
+    final response = await http.get(
+      Uri.parse('https://newsapi.org/v2/top-headlines?country=us&apiKey=44e183eb307d45648fa402c90d485a14'));
+      var jsonData = convert.jsonDecode(response.body) ;
+        print((jsonData['articles'] as List).map((articleJson) =>Article(title: articleJson['title'], description: articleJson['description'])).toList());
+      final articles = (jsonData['articles'] as List)
+          .map((articleJson) =>
+              Article(title: articleJson['title'], description: articleJson['description']))
+          .toList();
+      return articles;
+    } 
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false , 
+      home: Scaffold(
+        appBar: AppBar(title: Text('centered')),
+        body: FutureBuilder<List<Article>>(
+          future: fetchNews(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error loading news'));
+            }else {
+  final articles = snapshot.data!;
+              return ListView.builder(
+                itemCount: articles.length,
+                itemBuilder: (context, index) {
+                  final article = articles[index];
+                  return ListTile(
+                    title: Text(article.title),
+                    subtitle: Text(article.description),
+                  );
+                },
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
